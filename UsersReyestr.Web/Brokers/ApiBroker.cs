1f0d0dd1a28ad5d1e32f3c472f299c5a1de88e04
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2025-08-11 00:35:09 +0500

    API dan datalar olindi va Frontga chiqarildi

---
+++ UsersReyestr.Web/Brokers/ApiBroker.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Configuration;
+using RESTFulSense.Clients;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace UsersReyestr.Web.Brokers
+{
+    public partial class ApiBroker: IApiBroker
+    {
+        private readonly HttpClient httpClient;
+        private readonly IRESTFulApiFactoryClient restfulApiFactoryClient;
+        private readonly IConfiguration configuration;
+
+        public ApiBroker(
+            HttpClient httpClient,
+            IConfiguration configuration)
+        {
+            this.httpClient = httpClient;
+            this.configuration = configuration;
+
+            this.restfulApiFactoryClient =
+                GetRESTFulApiClient();
+        }
+        private async ValueTask<T> GetAsync<T>(string relativeUrl) =>
+           await this.restfulApiFactoryClient.GetContentAsync<T>(relativeUrl);
+
+        private IRESTFulApiFactoryClient GetRESTFulApiClient()
+        {
+            string apiBaseUrl =
+                this.configuration.GetValue<string>("ApiSettings:BaseUrl");
+
+            if (string.IsNullOrWhiteSpace(apiBaseUrl))
+                throw new InvalidOperationException("API BaseUrl konfiguratsiyada topilmadi.");
+
+            this.httpClient.BaseAddress = new Uri(apiBaseUrl);
+
+            return new RESTFulApiFactoryClient(this.httpClient);
+        }
+    }
+}
